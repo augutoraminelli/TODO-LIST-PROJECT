1e2ba131e95412318ba4f23e15808609f630c6ce
Guto Raminelli <augustoraminelli@gmail.com>
2022-05-20 09:44:53 -0300

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,62 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testing App.js',() => {
+  test('render input task', () => {
+    render(<App />);
+    const field = screen.getByTestId('input-task');
+
+    expect(field).toBeInTheDocument();
+  });
+
+  test('input recive value', () => {
+    render(<App />);
+    const field = screen.getByRole('textbox', { name: /task/i });
+    const value = 'test';
+    fireEvent.change(field, { target: { value } });
+
+    expect(field.value).toBe(value);
+  });
+
+  test('render button TASK to create', () => {
+    render(<App />);
+    const button = screen.getByRole('button', { name: /task/i });
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute('type', 'button');
+  });
+
+  test('onClick button created new task', () => {
+    render(<App />);
+    const field = screen.getByRole('textbox', { name: /task/i });
+    const button = screen.getByRole('button', { name: /task/i });
+    const value = 'test';
+    
+    fireEvent.change(field, { target: { value } });
+    fireEvent.click(button);
+
+    expect(screen.getByText(value)).toBeInTheDocument();
+  });
+
+  test('test method create write a few tasks at list', () => {
+   const todoList = ['Arrumar a cama', 'Tomar banho', 'Estudar React'];
+    render(<App />);
+    const field = screen.getByRole('textbox', { name: /task/i });
+    const button = screen.getByRole('button', { name: /task/i });
+
+    todoList.forEach(task => {
+      fireEvent.change(field, { target: { value: task } });
+      fireEvent.click(button);
+      expect(screen.getByText(task)).toBeInTheDocument();
+    });
+  });
+
+  test('render button DELETE to delete at each item', () => {
+    render(<App />);
+    const button = screen.getByRole('button', { name: /delete/i });
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute('type', 'button');
+  });
 });
